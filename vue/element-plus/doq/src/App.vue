bf505a561b1d3627ec501b280da488f455911db3
liheng <liheng2137@126.com>
2026-03-17 00:16:43 +0800

    Signed-off-by: liheng <liheng2137@126.com>

---
+++ vue/element-plus/doq/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const handleSelect = (key, keyPath) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<template>
+  <el-menu
+    :default-active="activeIndex"
+    mode="horizontal"
+    :ellipsis="false"
+    @select="handleSelect"
+  >
+    <el-menu-item index="1">首页</el-menu-item>
+    <el-menu-item index="2">产品中心</el-menu-item>
+    <el-menu-item index="3">解决方案</el-menu-item>
+    <el-menu-item index="4">关于我们</el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="5">登录</el-menu-item>
+    <el-menu-item index="6">注册</el-menu-item>
+  </el-menu>
+</template>
+
+<style scoped>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
